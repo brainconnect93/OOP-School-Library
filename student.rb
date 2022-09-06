cf6194cea1d2171ba4b0d968cf651d85c1537373
brainconnect93 <brainconnect0@gmail.com>
2022-09-06 19:26:07 +0100

    Add the teacher file and inserted some code

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require './person.rb'
 
-class Student
+class Student < Person
   def initialize(classroom = "Unknown")
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    
+    "¯\(ツ)/¯"
   end
 end
