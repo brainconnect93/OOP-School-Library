7cf6faa9bb1518645a1155653414c1ec5d7d1458
brainconnect93 <brainconnect0@gmail.com>
2022-09-06 20:31:43 +0100

    Fix rubocop errors

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :name, :age
   attr_reader :id
-  
+
   def initialize(age, name = 'Unkonwn', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
