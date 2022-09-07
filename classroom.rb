4076fac0d10e4116080af34035d7700e905f1a6a
brainconnect93 <brainconnect0@gmail.com>
2022-09-08 00:51:37 +0100

    Fix rubocop error

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
   attr_accessor :label, :student
 
-    def initialize(label)
-      @label = label
-      @students = []
-    end
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    def add_student(student)
-      @students << student
-      student.classroom = self
-    end
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
