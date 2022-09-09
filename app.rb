77c785e44a508f99cfd773b9fb66cfb0a1d68a35
brainconnect93 <brainconnect0@gmail.com>
2022-09-09 16:45:03 +0100

    Add the parameters to the app.rb file

--- app.rb
+++ app.rb
@@ -5,10 +5,64 @@ require './rental'
 
 class App
   attr_accessor :books, :teachers, :students, :rentals
+
     def initialize
       @books = []
       @teachers = []
       @students = []
       @rentals = []
     end
+
+    def list_books
+      @books.each_with_index { |book, n| print "(#{n}) Title: \"#{book.title}\", Author: \"#{book.author}\"\n"}
+    end
+
+    def list_people
+      people = [*@teachers, *@students]
+      people.each_with_index { |people, i| print "(#{i}) Name: \"#{people.name}\", Age: \"#{people.age}\"\n"}
+    end
+
+    def create_person
+      print "Do you want to create a student (1) or a teacher (2)? [input a number]:"
+      input = gets.chomp.to_i
+
+     if input == 1
+       print "Age"
+       age = gets.chomp.to_i
+       print "Name"
+       name = gets.chomp
+       print "Enter Classroom"
+       classroom = gets.chomp
+       new_student = Student.new(age, classroom)
+       new_student.name = name
+       @students << new_student
+       puts "#{new_student.name} added successfully"
+
+     elsif input == 2
+       print "Teacher Age"
+       age = gets.chomp.to_i
+       print "Name"
+       name = gets.chomp
+       print "Specialization"
+       specialization = gets.chomp
+       new_teacher = Teacher.new(age, name, specialization)
+       @teachers << new_teacher
+       puts "#{new_teacher.name} added successfully"
+    end
+  end
 end
+
+app = App.new
+
+# teacher = Teacher.new("age", "name", "specialization")
+# app.teachers.push(teacher)
+# app.create_person
+
+# Afolabi = Student.new(20, "classroom")
+# app.students.push(Afolabi)
+# app.list_people
+
+# books = Book.new("Home alone", "Afolabi")
+# app.books.push(books)
+# app.list_books
+
