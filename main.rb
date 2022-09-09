44d3f6cc9670f3fd3a336008ed3e0dbd0b4d73d0
brainconnect93 <brainconnect0@gmail.com>
2022-09-09 21:34:33 +0100

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,75 +1,43 @@
 require './app'
 
 class Main
-  def initialize
-    @app = App.new
-    @output = 0
-  end
-
-  def options
-    print 'WELCOME TO THE SCHOOL LIBRARY APP'
-    while @output != 7
-      print "\nPlease choose an option by entering a number \n\n"
-      print "1 - List all books \n"
-      print "2 - List all people \n"
-      print "3 - Create a person (teacher or student) \n"
-      print "4 - Create a book \n"
-      print "5 - Create a rental \n"
-      print "6 - List all rentals for a person \n"
-      print "7 - Quit \n"
-      start_app
+    def initialize
+      @app = App.new
+      @output = 0
     end
-  end
+    def options
+      print  "WELCOME TO THE SCHOOL LIBRARY APP"
+      while @output != 7
+        print "\nPlease choose an option by entering a number \n\n"
+        print "1 - List all books \n"
+        print "2 - List all people \n"
+        print "3 - Create a person (teacher or student) \n"
+        print "4 - Create a book \n"
+        print "5 - Create a rental \n"
+        print "6 - List all rentals for a person \n"
+        print "7 - Quit \n"
+        start_app
+       end 
+      end
 
-  def start_app
-    @output = gets.chomp.to_i
-    case @output
-    when 1
-      @app.list_books
-    when 2
-      @app.list_people
-    when 3
-      @app.create_person
-    when 4
-      @app.create_book
-    when 5
-      @app.create_rental
-    when 6
-      @app.list_rentals
-    end
-  end
+      def start_app
+        @output = gets.chomp.to_i
+        case @output
+        when 1
+          @app.list_books
+        when 2
+          @app.list_people
+        when 3
+          @app.create_person
+        when 4
+          @app.create_book
+        when 5
+          @app.create_rental
+        when 6
+          @app.list_rentals
+        end
+      end     
 end
 
 main = Main.new
 main.options
-
-# def functional(app)
-#   user_input = options
-#   case user_input
-#   when 1
-#     app.
-#     functional(app)
-#   when 2
-#     app.list_people
-#     functional(app)
-#   when 3
-#     app.create_person
-#     functional(app)
-#   when 4
-#     app.create_book
-#     functional(app)
-#   when 5
-#     app.create_rental
-#     functional(app)
-#   when 6
-#     app.list_rentals
-#     functional(app)
-#   end
-# end
-
-# def main
-#   app = App.new
-#   functional(app)
-# end
-
-# main
