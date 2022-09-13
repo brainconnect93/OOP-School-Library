bee4c1ac541b56b4436e6bf8672016e928fcbc96
brainconnect93 <brainconnect0@gmail.com>
2022-09-13 09:12:21 +0100

    Refactor the create book and remove it from the app file

---
+++ create_book.rb
@@ -0,0 +1,15 @@
+class Create_book
+    def initialize(book)
+      @books = book
+    end
+
+    def create_book
+        print 'Book Title:'
+        title = gets.chomp
+        print 'Book Author:'
+        author = gets.chomp
+        new_book = Book.new(title, author)
+        @books << new_book
+        puts "#{new_book.title} created successfully"
+    end
+end
