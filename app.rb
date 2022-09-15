70924c07bf410be70ebc57ddd8b4a04c87933093
Kamba56 <tomajohn316@yahoo.com>
2022-09-15 20:59:54 +0100

    Create json file

--- app.rb
+++ app.rb
@@ -7,12 +7,17 @@ class App
   attr_accessor :books, :people, :rentals
 
   def initialize
-    books_file = File.open('books.json', 'r')
+    books_file = create_json('books')
     @books = books_file ? JSON.parse(books_file.read, create_additions: true) : []
-    people_file = File.open('people.json', 'r')
+    people_file = create_json('people')
     @people = people_file ? JSON.parse(people_file.read, create_additions: true) : []
-    rent_file = File.open('rent.json', 'r')
+    rent_file = create_json('rent')
     @rentals = rent_file ? JSON.parse(rent_file.read, create_additions: true) : []
+  end
+
+  def create_json(string)
+    File.write("#{string}.json", []) unless File.exist?("#{string}.json")
+    File.open("#{string}.json", 'r')
   end
 
   def list_books
--- rental.rb
+++ rental.rb
@@ -4,9 +4,7 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    # @book.rentals << self
     @person = person
-    # @person.rentals << self
   end
 
   def to_json(*args)
