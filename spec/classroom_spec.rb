d926ab57a0a48dfc68299487c35db5912785778e
brainconnect93 <brainconnect0@gmail.com>
2022-09-16 16:37:30 +0100

    Fix rubocop errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe Classroom do
-    before :each do
-        @classroom = Classroom.new("students")
-    end
+  before :each do
+    @classroom = Classroom.new('students')
+  end
 
-    describe "#new" do
-        it "Should return a Classroom object" do
-            expect(@classroom).to be_an_instance_of Classroom
-        end
+  describe '#new' do
+    it 'Should return a Classroom object' do
+      expect(@classroom).to be_an_instance_of Classroom
     end
+  end
 
-    describe "#students" do
-        it "should return student in classroom" do
-            expect(@classroom.students).to eql []
-        end
+  describe '#students' do
+    it 'should return student in classroom' do
+      expect(@classroom.students).to eql []
     end
+  end
 end
